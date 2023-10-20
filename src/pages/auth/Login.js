39df018914265b01376498f1b2c13755c26d4f6a
SanikaMore <sanikam1543@gmail.com>
2023-10-20 14:40:33 +0530

    Changes

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import axios from 'axios';
 import './Login.css'; // Import your CSS file
-import { useNavigate, Link } from 'react-router-dom'; 
+import { useNavigate, Link } from 'react-router-dom';
+import { GoogleLogin } from '@react-oauth/google';
 
 function Login() {
     const initialValues = {
@@ -12,30 +13,33 @@ function Login() {
 
     const [isLoggedIn, setLoggedIn] = useState(false);
 
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
 
-    const handleSubmit =  (values, { resetForm }) => {
+    const handleSubmit = (values, { resetForm }) => {
         axios
             .post('http://127.0.0.1:8000/api/user/login/', values)
             .then((response) => {
-                console.log(values,response);
+                console.log(values, response);
                 sessionStorage.setItem('user', JSON.stringify(response.data.user));
                 alert('Login successful');
-                setLoggedIn(true); 
+                setLoggedIn(true);
             })
             .catch((error) => {
-                alert('Login failed. Enter correct credentials'); 
+                alert('Login failed. Enter correct credentials');
                 resetForm();
             });
     };
 
-    if (isLoggedIn) {
-        navigate('/');
-    }
+    const handleGoogleLoginSuccess = (credentialResponse) => {
+        console.log(credentialResponse);
+        alert('Login successful');
+        setLoggedIn(true); // Set the login status to true
+        navigate('/'); // Navigate to the home page
+    };
 
     return (
         <div className="login-container">
-            <h2 className='text-dark'>LOGIN</h2>
+            <h2 className="text-dark">LOGIN</h2>
             <Formik initialValues={initialValues} onSubmit={handleSubmit}>
                 <Form>
                     <div>
@@ -48,13 +52,22 @@ function Login() {
                         <Field type="password" id="password" name="password" />
                         <ErrorMessage name="password" component="div" />
                     </div>
-                    <div className='space'></div>
-                    <button type="submit" className='submit-auth-btn'>Login</button>
+                    <div className="space"></div>
+                    <button type="submit" className="submit-auth-btn">
+                        Login
+                    </button>
                 </Form>
             </Formik>
             <div className="register-link">
                 Don't have an account? <Link to="/register">Register here</Link>
             </div>
+
+            <GoogleLogin
+                onSuccess={handleGoogleLoginSuccess}
+                onError={() => {
+                    console.log('Login Failed');
+                }}
+            />
         </div>
     );
 }
